907fbf897465a00985dbfdb3b412368f4a65cb89
LuckyJQ <shenjianqi1111@126.com>
2017-09-03 23:34:19 +0800

    Site updated: 2017-09-03 23:34:18

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://github.com/LuckyJQ/luckyQ/min_photos/' + data.link[i];
-          var src = 'https://github.com/LuckyJQ/luckyQ/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/LuckyJQ/LuckyQ/master/min_photos/' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/LuckyJQ/LuckyQ/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
